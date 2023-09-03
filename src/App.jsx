f2b7d829854718e26132ea3274432cec10ca9d14
ahdianmirza <ahdianmirza@gmail.com>
2023-09-03 19:52:30 +0700

    first commit react + tailwind practice project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center font-bold bg-blue-600 min-h-screen items-center">Hello World</div>
+    </>
+  )
+}
+
+export default App
