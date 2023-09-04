b50dd191533f43a1e46874fc6c0b7c9acfde9684
ahdianmirza <ahdianmirza@gmail.com>
2023-09-04 18:21:15 +0700

    Conditional rendering

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,17 +1,40 @@
+import { Link } from "react-router-dom";
+
 const AuthLayouts = (props) => {
-    const { children, title } = props;
+    const { children, title, type } = props;
 
     return (
-        <div className="flex justify-center font-bold min-h-screen items-center">
+        <div className="flex justify-center min-h-screen items-center">
             <div className="w-full max-w-xs">
             <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
             <p className="font-medium text-slate-500 mb-8">
                 Welcome, Please enter your detail
             </p>
             {children}
+            <Navigation type={type}></Navigation>
             </div>
         </div>
     );
 };
+
+const Navigation = ({type}) => {
+    if (type === "login") {
+        return (
+            <p className="text-sm font-semibold mt-5 text-center">
+                Don't have an account?{" "}
+                <Link to="/register" className="font-bold text-blue-600">Sign Up!</Link>
+            </p>
+        );
+    } else {
+        return (
+            <p className="text-sm font-semibold mt-5 text-center">
+                Already have an account?{" "}
+                <Link to="/login" className="font-bold text-blue-600">
+                Sign In!
+                </Link>
+            </p>
+        );
+    }
+}
 
 export default AuthLayouts;
