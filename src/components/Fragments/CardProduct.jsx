7d0ebe135b732729de9b928e153e981de6a6dacd
ahdianmirza <ahdianmirza@gmail.com>
2023-09-10 21:48:40 +0700

    useState

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -4,7 +4,7 @@ const CardProduct = (props) => {
     const { children } = props;
 
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
+        <div className="w-full max-w-xs bg-gray-800 border border-gray-700 rounded-lg shadow mx-3 my-2 flex flex-col justify-between">
             {children}
         </div>
     );
@@ -42,12 +42,12 @@ const Body = (props) => {
 }
 
 const Footer = (props) => {
-    const { price } = props;
+    const { price, handleAddToChart, id } = props;
 
     return (
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">{price}</span>
-            <Button classname="bg-blue-600">Add to Cart</Button>
+            <span className="text-xl font-bold text-white">Rp {price.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</span>
+            <Button classname="bg-blue-600" onClick={() => handleAddToChart(id)}>Add to Cart</Button>
         </div>
     );
 }
